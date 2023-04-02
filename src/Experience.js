2a4a1ba740111862ff516168e82acb85420c3b2d
chigginss <cierramhiggins@gmail.com>
2023-04-02 15:00:22 +1200

    Update contact me and experience details

--- src/Experience.js
+++ src/Experience.js
@@ -19,6 +19,7 @@ export default function Experience() {
     <div id='experience' className={`experience fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={domRef}>
       <div className='leftTitle'>Experience</div>
       <p className='aboutMeText'> I have 3+ years experience in Web, iOS and Android development. Recently, I worked at <a className='link' href="https://hbomax.com" target="_blank" rel="noreferrer">HBO Max</a> and previously at <a className='link' href="https://grokker.com" target="_blank" rel="noreferrer">Grokker.</a></p>
+      <p className='aboutMeText'>Currently, I'm working in New Zealand at <a className='link' href="https://boost.co.nz" target="_blank" rel="noreferrer">Boost.</a></p>
       <p className='aboutMeText'> I primarily work with JavaScript (React and React Native), Typescript, and MongoDB. I've worn many hats throughout the years, but I primarily focus on delivering accessible and intuitive user facing features. </p>
       <div className='resumeButtonContainer'>
           <a className='resumeButton' download='resume' href={Resume} rel="noreferrer" target='_blank'> DOWNLOAD RESUME</a>
